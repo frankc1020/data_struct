3eedb4644678d73301273372dd9e8eb23d3d0294
guzc <gucong19961020@163.com>
2021-03-17 21:44:31 +0800

    测试

--- src/test/java/niu/ke/com/NearValueTestDemo.java
+++ src/test/java/niu/ke/com/NearValueTestDemo.java
@@ -1,5 +1,7 @@
 package niu.ke.com;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -12,12 +14,25 @@ import java.util.Scanner;
 public class NearValueTestDemo {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+       /* Scanner in = new Scanner(System.in);
         double a = in.nextDouble();
         int b = (int)(a+0.5);
 
         System.out.println(b);
-        Math.round(100);
+        Math.round(100);*/
+
+       boolean flag = false;
+       List<Long> arr = new ArrayList<Long>();
+       arr.add(1000000000091201L);
+        List<Long> arr2 = new ArrayList<Long>();
+        arr2.add(1000000000091201L);
+        if(arr.containsAll(arr2) && arr2.containsAll(arr)){
+            flag = true;
+        }
+        System.out.println(flag);
+
+
+
     }
 
 }
